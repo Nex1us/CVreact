57d0a09db6ced954699f9fecfd93b3cd2135e705
Nex1us <filgueiraa985@gmail.com>
2024-06-24 11:30:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+
+function App() {
+  return (
+<>
+  <h1>Curriculum Vitae</h1>
+  <BrowserRouter>
+  <Routes>
+<Route></Route>
+
+  </Routes>
+  
+  </BrowserRouter>
+</>
+  );
+}
+
+export default App;
